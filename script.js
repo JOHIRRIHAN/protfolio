e684e726ef823210df654223633a1a3d8fac8fd3
JOHIRRIHAN <jr7718301@gmail.com>
2024-03-16 19:43:45 +0600

    responsive ok

---
+++ script.js
@@ -0,0 +1,26 @@
+function mailSend() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+
+    // Sending email using EmailJS
+    emailjs.send("service_qe5hbep", "template_ek2em98", parms)
+        .then(function(response) {
+            alert("Send Successful!! Click ok!!!");
+            clearForm(); // Clear the form after successful submission
+        }, function(error) {
+            console.error('EmailJS send error:', error);
+            alert("Failed to send email. Please try again later.");
+        });
+}
+
+function clearForm() {
+    // Clearing input fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+}
